19b24e9dd3e410f710d6207f0fa4624557b2004f
Huy Tong <mr.cukingpro@gmail.com>
2023-11-20 21:11:05 +0700

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RxSVProgressHUD",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "RxSVProgressHUD",
+            targets: ["RxSVProgressHUD"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
+        .package(url: "https://github.com/SVProgressHUD/SVProgressHUD.git", branch: "master"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "RxSVProgressHUD",
+            dependencies: [
+                "RxSwift",
+                .product(name: "RxCocoa", package: "RxSwift"),
+                "SVProgressHUD",
+            ]),
+        .testTarget(
+            name: "RxSVProgressHUDTests",
+            dependencies: ["RxSVProgressHUD"]),
+    ])
